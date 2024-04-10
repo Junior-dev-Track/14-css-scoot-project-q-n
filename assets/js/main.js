9a47c99bbae7cddc056f5288dc776fbad961042e
Qheuss <quentin.heusse@gmail.com>
2024-04-10 10:02:22 +0200

    home grid most part done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
+`use strict`;
 
+// header movements
+const header = document.querySelector('header');
+const main = document.querySelector('main');
+let prevScrollPos = window.scrollY;
+
+window.onscroll = function () {
+  let currentScrollPos = window.scrollY;
+
+  // Scroll down
+  if (prevScrollPos < currentScrollPos) {
+    header.classList.add('header-hidden');
+    header.classList.remove('header-visible');
+    // main.classList.remove('paddingMain');
+  }
+  // Scroll up
+  else {
+    header.classList.add('header-visible');
+    header.classList.remove('header-hidden');
+    // main.classList.add('paddingMain');
+  }
+
+  prevScrollPos = currentScrollPos;
+};
