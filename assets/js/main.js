863464d8be4cc2e49daedd22695c209ebef9003e
natallya-K <kavalkova@yahoo.fr>
2024-04-11 20:11:10 +0200

    accordion arrows added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 `use strict`;
 
 // header movements
-const header = document.querySelector('header');
-const main = document.querySelector('main');
+const header = document.querySelector("header");
+const main = document.querySelector("main");
 let prevScrollPos = window.scrollY;
 
 window.onscroll = function () {
@@ -10,30 +10,30 @@ window.onscroll = function () {
 
   // Scroll down
   if (prevScrollPos < currentScrollPos) {
-    header.classList.add('header-hidden');
-    header.classList.remove('header-visible');
+    header.classList.add("header-hidden");
+    header.classList.remove("header-visible");
   }
   // Scroll up
   else {
-    header.classList.add('header-visible');
-    header.classList.remove('header-hidden');
+    header.classList.add("header-visible");
+    header.classList.remove("header-hidden");
   }
 
   prevScrollPos = currentScrollPos;
 };
 
 //Accordion
-let acc = document.getElementsByClassName('accordion');
-let i;
+var acc = document.getElementsByClassName("accordion");
+var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener('click', function () {
-    this.classList.toggle('active');
-    let panel = this.nextElementSibling;
-    if (panel.style.display === 'block') {
-      panel.style.display = 'none';
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
     } else {
-      panel.style.display = 'block';
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
 }
